ec95f47a8269413e0dac20ca8c22b1b84e6d7345
HAZEL <hyezico@outlook.com>
2018-03-18 17:43:50 -0400

    Making quiz marker, the component should be smart

--- src/components/QuizDemonym.js
+++ src/components/QuizDemonym.js
@@ -5,18 +5,24 @@ const QuizDemonym = (props) => {
     const countryObjLength = countryObj.length;
     const randomQuiz = Math.floor(Math.random() * countryObjLength);
     const randomAnswerGenerator = (number) => Math.floor(Math.random() * number);
-    const answerArr = [countryObj[randomQuiz].demonym, countryObj[randomAnswerGenerator(countryObjLength)].demonym, countryObj[randomAnswerGenerator(countryObjLength)].demonym];
+    const answerArr = [
+        countryObj[randomQuiz].demonym,
+        countryObj[randomAnswerGenerator(countryObjLength)].demonym,
+        countryObj[randomAnswerGenerator(countryObjLength)].demonym,
+        countryObj[randomAnswerGenerator(countryObjLength)].demonym,
+        countryObj[randomAnswerGenerator(countryObjLength)].demonym
+    ];
     const shuffle = (array) => {
         let counter = array.length;
-        
+
         // While there are elements in the array
         while (counter > 0) {
             // Pick a random index
             let index = Math.floor(Math.random() * counter);
-            
+
             // Decrease counter by 1
             counter--;
-            
+
             // And swap the last element with it
             let temp = array[counter];
             array[counter] = array[index];
@@ -24,16 +30,14 @@ const QuizDemonym = (props) => {
         }
         return array;
     }
-    console.log(answerArr);
-    // Pseudocode:
-    // 1. Put the data inside an array of objects (NOT hardcoded in the JSX)
-    // 1 correct + 2 random
-
-    // 2. Make another variable which randomizes the order of that array
-
-    // 3. Render that array by mapping through it in the JSX below
-    // Only write <input> ONCE
-
+    const showReview = false;
+    const onClick = (e) => {
+        console.log(e.target.value);
+        console.log(countryObj[randomQuiz].demonym);
+        if(e.target.value === countryObj[randomQuiz].demonym){
+            console.log('Youre right!');
+        }
+    }
     return (
         <div>
             <h1>{countryObj[randomQuiz].name}의 국민들은 영어로 어떻게 말할까요?</h1>
@@ -42,10 +46,11 @@ const QuizDemonym = (props) => {
             {
                 shuffle(answerArr).map(answer => (
                     <div>
-                    <input type="checkbox"/><label>{answer}</label>
+                        <button value={answer} onClick={onClick}>{answer}</button>
                     </div>
                 ))
             }
+            {showReview && <h1>Try again</h1>}
         </div>
     )
 }
