350eaea665e2a89fdc26ed1e39401cb7085dbcd3
HAZEL <hyezico@outlook.com>
2018-03-19 23:42:55 -0400

    Swithed with smart component on quizdemonym page

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,31 +5,21 @@ import GoBack from './GoBack';
 import QuizDemonym from './QuizDemonym';
 import QuizCountry from './QuizCountry';
 
-class Quiz extends React.Component {
-    constructor(props) {
-        super(props);
-        let match = props.match;
-        this.state = {
-            nations: Data,
-        };
-    }
-    render() {
-        console.log(this.props.match);
-        return (
-            <div>
-                <GoBack />
-                <ul>
-                    <li>
-                        <Link to={`${this.props.match.url}/demonym`}>국민들</Link>
-                    </li>
-                    <li>
-                        <Link to={`${this.props.match.url}/country`}>나라들</Link>
-                    </li>
-                </ul>
-                <Route path={`${this.props.match.url}/demonym`} component={() => <QuizDemonym nation={this.state.nations}/>} />
-                <Route path={`${this.props.match.url}/country`} component={() => <QuizCountry nation={this.state.nations}/>} />
-            </div>
-        );
-    }
+const Quiz = props => {
+    return (
+        <div>
+            <GoBack />
+            <ul>
+                <li>
+                    <Link to={`${props.match.url}/demonym`}>국민들</Link>
+                </li>
+                <li>
+                    <Link to={`${props.match.url}/country`}>나라들</Link>
+                </li>
+            </ul>
+            <Route path={`${props.match.url}/demonym`} component={QuizDemonym} />
+            <Route path={`${props.match.url}/country`} component={QuizCountry} />
+        </div>
+    );
 }
 export default Quiz;
--- src/components/QuizDemonym.js
+++ src/components/QuizDemonym.js
@@ -1,61 +1,62 @@
-import React from "react";
-
-const QuizDemonym = (props) => {
-    const countryObj = Object.values(props.nation);
-    const countryObjLength = countryObj.length;
-    const randomQuiz = Math.floor(Math.random() * countryObjLength);
-    const randomAnswerGenerator = (number) => Math.floor(Math.random() * number);
-    const answerArr = [
-        countryObj[randomQuiz].demonym,
-        countryObj[randomAnswerGenerator(countryObjLength)].demonym,
-        countryObj[randomAnswerGenerator(countryObjLength)].demonym,
-        countryObj[randomAnswerGenerator(countryObjLength)].demonym,
-        countryObj[randomAnswerGenerator(countryObjLength)].demonym
-    ];
-    const shuffle = (array) => {
-        let counter = array.length;
-
-        // While there are elements in the array
-        while (counter > 0) {
-            // Pick a random index
-            let index = Math.floor(Math.random() * counter);
-
-            // Decrease counter by 1
-            counter--;
-
-            // And swap the last element with it
-            let temp = array[counter];
-            array[counter] = array[index];
-            array[index] = temp;
-        }
-        return array;
+import React, { Component } from 'react';
+import Data from '../countrydata';
+class QuizDemonym extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            countryObj: Object.values(Data),
+            showReview: false,
+            randomQuiz: Math.floor(Math.random() * 250),
+        };
     }
-    const showReview = false;
-    const onClick = (e) => {
-        console.log(e.target.value);
-        console.log(countryObj[randomQuiz].demonym);
-        if(e.target.value === countryObj[randomQuiz].demonym){
-            console.log('Youre right!');
+    onClick = (e) => {
+        if (e.target.value === this.state.countryObj[this.state.randomQuiz].demonym) {
+            this.setState({ showReview: true })
         }
     }
-    return (
-        <div>
-            <h1>{countryObj[randomQuiz].name}의 국민들은 영어로 어떻게 말할까요?</h1>
-            <img width="200px" src={countryObj[randomQuiz].flag} />
-            <h2>{countryObj[randomQuiz].name}</h2>
-            {
-                shuffle(answerArr).map(answer => (
-                    <div>
-                        <button value={answer} onClick={onClick}>{answer}</button>
-                    </div>
-                ))
+    render() {
+        const countryObjLength = this.state.countryObj.length;
+        const randomAnswerGenerator = (number) => Math.floor(Math.random() * number);
+        const answerArr = [
+            this.state.countryObj[this.state.randomQuiz].demonym,
+            this.state.countryObj[randomAnswerGenerator(countryObjLength)].demonym,
+            this.state.countryObj[randomAnswerGenerator(countryObjLength)].demonym,
+            this.state.countryObj[randomAnswerGenerator(countryObjLength)].demonym,
+            this.state.countryObj[randomAnswerGenerator(countryObjLength)].demonym
+        ];
+        const shuffle = (array) => {
+            let counter = array.length;
+
+            // While there are elements in the array
+            while (counter > 0) {
+                // Pick a random index
+                let index = Math.floor(Math.random() * counter);
+
+                // Decrease counter by 1
+                counter--;
+
+                // And swap the last element with it
+                let temp = array[counter];
+                array[counter] = array[index];
+                array[index] = temp;
             }
-            {showReview && <h1>Try again</h1>}
-        </div>
-    )
+            return array;
+        }
+        return (
+            <div>
+                <h1>{this.state.countryObj[this.state.randomQuiz].name}의 국민들은 영어로 어떻게 말할까요?</h1>
+                <img width="200px" src={this.state.countryObj[this.state.randomQuiz].flag} />
+                <h2>{this.state.countryObj[this.state.randomQuiz].name}</h2>
+                {
+                    shuffle(answerArr).map(answer => (
+                        <div>
+                            <button value={answer} onClick={this.onClick}>{answer}</button>
+                        </div>
+                    ))
+                }
+                {this.state.showReview ? <h1>you got it right</h1> : <h1>Try again</h1>}
+            </div>
+        );
+    }
 }
-
-
-
-
 export default QuizDemonym;
